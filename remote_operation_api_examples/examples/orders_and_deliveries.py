a224c85a185a3002bc01178e51bad41dc3e5e47a
Simone Robutti <simone.robutti@protonmail.com>
2021-11-05 09:27:36 +0100

    added order and deliveries example

--- remote_operation_api_examples/examples/orders_and_deliveries.py
+++ remote_operation_api_examples/examples/orders_and_deliveries.py
@@ -20,3 +20,31 @@ def create_order(config: Config):
     response.raise_for_status()
     print("Order successfully created")
     return response.json()["id"]
+
+
+def create_delivery(config: Config):
+    """
+    This example shows how to create a delivery with two orders
+    """
+
+    # we create two orders
+    order_id_1 = create_order(config)
+    order_id_2 = create_order(config)
+
+    # we update their status to READY_DELIVERY, meaning that they are ready to be included in a delivery and picked up
+    request_body = {"order_status": "READY_DELIVERY"}
+    response = config.session.post(url=urljoin(config.platform_url, f"orders/{order_id_1}/status"), json=request_body)
+    response.raise_for_status()
+    response = config.session.post(url=urljoin(config.platform_url, f"orders/{order_id_2}/status"), json=request_body)
+    response.raise_for_status()
+
+    # we then create a delivery using a demo driver id and a demo vehicle
+    body = {
+        "order_ids": [order_id_1, order_id_2],
+        "vehicle_id": "608b17eb-78b8-45e5-835f-053e7ba93e72",
+        "driver_id": "378169e0-920e-4256-913c-b2536d20ad00",
+    }
+    response = config.session.post(url=urljoin(config.platform_url, "deliveries"), json=body)
+    response.raise_for_status()
+
+    print("Delivery successfully created")
